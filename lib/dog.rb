3ad1d9f716b1c42d191fbf95fd88f9c5e2620a20
Jared  E <jared.epps322@gmail.com>
2020-05-09 00:32:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,61 @@
-class Dog
 
-def name=(name)
-  @name = name 
-end
-
-def name
-  @name 
+class Dog 
+  
+  def name 
+    @name 
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  
+  
+  
+  
+  
 end 
 
-def breed=(breed)
-  @breed = breed 
-end 
 
-def breed
-  @breed 
-end 
 
-end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+
+# def name=(name)
+#   @name = name 
+# end
+
+# def name
+#   @name 
+# end 
+
+# def breed=(breed)
+#   @breed = breed 
+# end 
+
+# def breed
+#   @breed 
+# end 
+
+# end 
 
 
 
